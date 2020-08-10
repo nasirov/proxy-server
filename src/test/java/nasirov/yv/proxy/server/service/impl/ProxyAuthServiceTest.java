703dedae07d2787fe3313069d22fe0863e65a678
hikka <yurqaa@mail.ru>
2020-08-10 14:36:25 +0300

    add ProxyAuthServiceTest
    1. add system-lambda in order to testing with system environment variables
    2. add ProxyAuthServiceTest

---
+++ src/test/java/nasirov/yv/proxy/server/service/impl/ProxyAuthServiceTest.java
@@ -0,0 +1,55 @@
+package nasirov.yv.proxy.server.service.impl;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import com.github.stefanbirkner.systemlambda.SystemLambda;
+import lombok.SneakyThrows;
+import nasirov.yv.proxy.server.service.ProxyAuthServiceI;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by nasirov.yv
+ */
+public class ProxyAuthServiceTest {
+
+	private ProxyAuthServiceI proxyAuthService;
+
+	@Before
+	@SneakyThrows
+	public void setUp() {
+		SystemLambda.withEnvironmentVariable("PROXY_CREDENTIALS", "dGVzdDp0ZXN0")
+				.execute(() -> proxyAuthService = ProxyAuthService.getInstance());
+	}
+
+	@Test
+	public void shouldReturnTrue() {
+		//given
+		String rawHttp = "GET http://foo.bar HTTP/1.1\r\nUser-Agent: Foo-bar\r\nProxy-Authorization: Basic dGVzdDp0ZXN0\r\n";
+		//when
+		boolean result = proxyAuthService.isAuthorized(rawHttp);
+		//then
+		assertTrue(result);
+	}
+
+	@Test
+	public void shouldReturnFalseInvalidCredentials() {
+		//given
+		String rawHttp = "GET http://foo.bar HTTP/1.1\r\nUser-Agent: Foo-bar\r\nProxy-Authorization: Basic foo\r\n";
+		//when
+		boolean result = proxyAuthService.isAuthorized(rawHttp);
+		//then
+		assertFalse(result);
+	}
+
+	@Test
+	public void shouldReturnFalseNoHeader() {
+		//given
+		String rawHttp = "GET http://foo.bar HTTP/1.1\r\nUser-Agent: Foo-bar\r\n";
+		//when
+		boolean result = proxyAuthService.isAuthorized(rawHttp);
+		//then
+		assertFalse(result);
+	}
+}
